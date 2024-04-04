1f8588ea32e89b8a6ac391e7b9f2b6de47722bc3
YukiharaSensei <nguyenhoang.moni03@gmail.com>
2024-04-04 08:38:01 +0700

    add bootstrap

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from "./components/Header.tsx";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <Header/>
     <App />
   </React.StrictMode>,
 )
