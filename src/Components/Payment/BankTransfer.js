9efaff53f6e4474449984eeb0793a005c233a62a
NguyenThoQuan <thoquan150903@gmail.com>
2024-04-30 13:52:23 +0700

    Fianl Update Payment Cash

--- src/Components/Payment/BankTransfer.js
+++ src/Components/Payment/BankTransfer.js
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default function BankTransfer() {
+export default function BankTransfer(props) {
+    const { listStatus } = props
+
     return (
-        <>Bank Transfer</>
+        <div className="bank-transfer">
+            <form>
+                <h2>Thanh toán bằng tiền mặt</h2>
+                <div>
+                    <div>Nhập số điện thoại</div>
+                    <input type="text" placeholder="Nhập số điện thoại khách hàng ..." value={phoneNumber} onChange={handlePhoneNumberChange} />
+                </div>
+                <div>
+                    <div>Chọn dịch vụ muốn thanh toán</div>
+                    <select onChange={handleServiceSelect}>
+                        <option>Chọn dịch vụ</option>
+                        {
+                            foundCustomer && foundCustomer.map((item) => {
+                                return (
+                                    <option key={item.id}>{item.service}</option>
+                                )
+                            })
+                        }
+                    </select>
+                </div>
+                <div>
+                    <div>Số tiền cần thanh toán</div>
+                    <input type="text" value={selectedQuote} readOnly />
+                </div>
+                <div>
+                    <div>Số tiền thanh toán</div>
+                    <input type="text" />
+                </div>
+                <div className="button">
+                    <button>Thanh toán</button>
+                </div>
+            </form>
+        </div>
     )
 }
--- src/Components/Payment/Cash.js
+++ src/Components/Payment/Cash.js
@@ -4,6 +4,7 @@ export default function Cash(props) {
     const { listStatus } = props;
     const [phoneNumber, setPhoneNumber] = useState("");
     const [foundCustomer, setFoundCustomer] = useState([]);
+    const [selectedQuote, setSelectedQuote] = useState("");
 
     const handlePhoneNumberChange = (event) => {
         const { value } = event.target;
@@ -14,7 +15,18 @@ export default function Cash(props) {
             let customer = listStatus.filter((item) => item.phone.includes(value));
             setFoundCustomer(customer);
         }
+        setSelectedQuote("");
     }
+
+    const handleServiceSelect = (event) => {
+        const selectedService = event.target.value;
+        const selectedCustomer = foundCustomer.find((item) => item.service === selectedService);
+        if (selectedCustomer) {
+            setSelectedQuote(selectedCustomer.quote);
+        } else {
+            setSelectedQuote("");
+        }
+    };
 
     return (
         <div className="cash">
@@ -26,7 +38,7 @@ export default function Cash(props) {
                 </div>
                 <div>
                     <div>Chọn dịch vụ muốn thanh toán</div>
-                    <select>
+                    <select onChange={handleServiceSelect}>
                         <option>Chọn dịch vụ</option>
                         {
                             foundCustomer && foundCustomer.map((item) => {
@@ -39,7 +51,7 @@ export default function Cash(props) {
                 </div>
                 <div>
                     <div>Số tiền cần thanh toán</div>
-                    <input type="text" />
+                    <input type="text" value={selectedQuote} readOnly />
                 </div>
                 <div>
                     <div>Số tiền thanh toán</div>
